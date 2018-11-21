21a992d62c8a02c480f9e16d31e114dfe1201701
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-21 16:18:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log("Welcome Again !");
-  return channel.send(
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-
-    {
-
-https://discord.gg/ZmmsvYp
-joined info-selfie
-! Selfie.
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-)
-}).catch(console.error)
-
-})
+console.log("BOT ONLINE");
  
-client.on("guildMemberAdd", member => {
+A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(
-
-    }
-
-    {
-    
-https://discord.gg/ZmmsvYp
-joined info-selfie
-! Selfie.
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+  return channel.send(**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/ZmmsvYp ] 
+الدعووة خاصة لك ... [ ${member}  ]
 **) 
 }).catch(console.error)
-}
+})
 client.login(process.env.BOT_TOKEN);
