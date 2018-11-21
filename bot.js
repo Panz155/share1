1d8475ed61f765b9ff8e1554e32f97a9ece9d1e6
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-21 15:51:38 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+  return channel.send(
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+
+
+رابط السيرفر
+joined info-selfie
+! Selfie.
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(
+
+
+
+
+رابط السيرفر
+https://discord.gg/ZmmsvYp
+joined info-selfie
+! Selfie.
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**) 
+}).catch(console.error)
+
+client.login(process.env.BOT_TOKEN);
